1e463d96ccc69de6648d18a71fa5dd5b5bf5a1bc
danielwo0212 <danielwo0212@gmail.com>
2026-01-16 13:30:40 +0200

    Update spread_fire.py

--- spread_fire.py
+++ spread_fire.py
@@ -3,12 +3,26 @@ def spread_fire(grid):
     update_grid = grid.copy()
     grid_size = len(grid)
 
-    for i in range(grid_size - 1):
-        for j in range(grid_size - 1):
-            if grid[i,j] == 1:
-                neighbors = [grid[i - 1,j], grid[i + 1,j], grid[i,j - 1], grid[i,j + 1]]
+    for i in range(grid_size):
+        for j in range(grid_size):
+          if grid[i,j] == 1 and i < 29 and j < 29:
+              neighbors = [grid[i - 1,j], grid[i + 1,j], grid[i,j - 1], grid[i,j + 1]]
+              if 2 in neighbors:
+                  update_grid[i,j] = 2
+
+          if i == 29 and grid[i,j] == 1 and j < 29:
+                neighbors = [grid[i - 1,j], grid[i,j - 1], grid[i,j + 1]]
                 if 2 in neighbors:
                     update_grid[i,j] = 2
 
-    return update_grid
+          if j == 29 and grid[i,j] == 1 and i < 29:
+              neighbors = [grid[i - 1,j], grid[i + 1,j], grid[i,j - 1]]
+              if 2 in neighbors:
+                  update_grid[i,j] = 2
 
+          if i == 29 and j == 29 and grid[i,j] == 1:
+              neighbors = [grid[i - 1,j], grid[i,j - 1]]
+              if 2 in neighbors:
+                  update_grid[i,j] = 2
+
+    return update_grid
